f38ae761c479e6af3470a484d15a3d2d0b7fca44
madholl <madholl@udel.edu>
2021-10-05 16:01:11 -0400

    updated fridge interface to create new fridge

--- pages/fridges/src/fridge.js
+++ pages/fridges/src/fridge.js
@@ -11,32 +11,23 @@ function selectFridge() {
     console.log("fridge selected");
 }
 
-function addImage(){
-    console.log("hello");
+function addImage(fridgeName){
     var table = document.getElementById("fridge-table")
     var emptyRow = document.getElementById("fridgeIcons");
     var fridgeColumn = document.getElementById("col1");
-    // .innerHTML="<img src='assets/fridgeIcon.png'>";
     if(emptyRow){
         emptyRow.parentNode.removeChild(emptyRow);
     }
-    // var newBowler = document.getElementsByName('bowlersName')[0].value;
-    // var tr = document.createElement('tr');
     var td = fridgeColumn.appendChild(document.createElement('td'));
-    td.innerHTML = "<img src='assets/fridgeIcon.png' onclick=selectFridge()>";
+    td.innerHTML = "<figure><img src='assets/fridgeIcon.png' onclick=selectFridge()/><figcaption>"+ fridgeName + "</figcaption></figure>";
+    console.log("made it!")
     table.appendChild(td);
 }
 
 function createNewFridge() {
-    console.log("modal pop up")
+    let closeBtn = document.querySelector(".close-btn")
     let modal = document.querySelector(".modal");
     modal.style.display = "block";
-    // let fridgeBtn = document.getElementById('newFridgeBtn');
-    // let modal = document.querySelector(".modal")
-    // let closeBtn = document.querySelector(".close-btn")
-    // fridgeBtn.onclick = function(){
-    //     modal.style.display = "block"
-    // }
     closeBtn.onclick = function(){
         modal.style.display = "none"
     }
@@ -47,29 +38,20 @@ function createNewFridge() {
     }
 }
 
-// function createNewFridge() {
-//     console.log("hello");
-//     $('#fridgeModal').on('show.bs.modal', function (event) {
-//     var button = $(event.relatedTarget) // Button that triggered the modal
-//     var recipient = button.data('whatever') // Extract info from data-* attributes
-//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//     var modal = $(this)
-//     // modal.find('.modal-title').text('New message to ' + recipient)
-//     modal.find('.modal-body input').val(recipient)
-//     })};
+function validateNewFridge() {
+    let fridgeName = document.getElementById("fridge-name-input").value.length;
+    console.log(fridgeName);
+    if (fridgeName > 0) {
+        let modal = document.querySelector(".modal");
+        console.log(fridgeName);
+        modal.style.display = "none";
+        addImage(document.getElementById("fridge-name-input").value);
+    }
+}
 
-//     let modalBtn = document.getElementById("modal-btn")
-//     let modal = document.querySelector(".modal")
-//     let closeBtn = document.querySelector(".close-btn")
-//         modalBtn.onclick = function(){
-//         modal.style.display = "block"
-//     }
-// closeBtn.onclick = function(){
-//   modal.style.display = "none"
-// }
-// window.onclick = function(e){
-//   if(e.target == modal){
-//     modal.style.display = "none"
-//   }
-// }
+window.onload = function() {
+    document.getElementById('new-fridge-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        return validateNewFridge();
+    });
+}
