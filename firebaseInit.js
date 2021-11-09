499ac0d75f9f74e1e3dc8935ae0f5e2f0947f400
mrpinkpigg <aaronknestaut@gmail.com>
2021-11-09 10:15:41 -0500

    whole buncha firebase changes (still a lot of code to edit)

---
+++ firebaseInit.js
@@ -0,0 +1,32 @@
+//IMPORTANT
+//ADD TO HTML BEFORE ANY OTHER <script> TAGS WITH:
+// <script src="firebaseInit.js" type="module"></script>
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCq0ziu2IydVWtN89EErVwdaQ0GDRlFELc",
+   authDomain: "fridgedaddy-ud21.firebaseapp.com",
+   databaseURL: "https://fridgedaddy-ud21-default-rtdb.firebaseio.com",
+   projectId: "fridgedaddy-ud21",
+   storageBucket: "fridgedaddy-ud21.appspot.com",
+   messagingSenderId: "50638166336",
+   appId: "1:50638166336:web:6c328b93575a53a565c2a5",
+   measurementId: "G-JBH3ETNLM2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ pages/profiles/profiles.js
@@ -0,0 +1,46 @@
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    
+    window.location.href = "myProfile.html"
+
+  } else {
+    // User is signed out
+    
+    window.location.href = "signIn.html"
+
+  }
+});
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+function login() {
+    var userEmail = document.getElementById("emailField").value;
+    var userPassword = document.getElementById("passwordField").value;
+
+    
+}
