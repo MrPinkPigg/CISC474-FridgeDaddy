eae4b2cc4101a9434c825176986942274fc1b4fe
Matthew Searfass <mattsea1028@gmail.com>
2021-11-27 11:34:23 -0500

    Updated results page

--- public/js/recipe-results.js
+++ public/js/recipe-results.js
@@ -1,10 +1,11 @@
+var ing_list;
 window.onload = function() {
-    var ing_list = JSON.parse(localStorage.getItem("ing_list"))
+    ing_list = JSON.parse(localStorage.getItem("ing_list"))
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3000/recipes',
         success: function(res) {
-
+            makeCards(res);
         },
         error: function(xhr, status, err) {
            console.log(xhr.responseText);
@@ -13,14 +14,16 @@ window.onload = function() {
  }
 
 function makeCards(obj) {
-    for(var i = 0; i < obj.recipe.length; i += 3) {
-        document.getElementById("list").innerHTML += '<div class="row" style="margin-left: 9em; margin-right: 9em;" ><div class="card-group"><div class="card"><img style="height: 18em !important;" src="' 
-            + obj.recipe[i].image +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' 
-            + obj.recipe[i].name + '</h5><p class="card-text">' + obj.recipe[i].description  + '</p></div><div class="card-footer text-center"><a href="#" class="btn btn-success">View</a></div></div><div class="card"><img style="height: 18em !important;" src="' 
-            + obj.recipe[i + 1].image +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' 
-            + obj.recipe[i + 1].name + '</h5><p class="card-text">' + obj.recipe[i + 1].description  + '</p></div><div class="card-footer text-center"><a href="#" class="btn btn-success">View</a></div></div><div class="card"><img style="height: 18em !important;"src="'
-            + obj.recipe[i + 2].image +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'
-            + obj.recipe[i + 2].name + '</h5><p class="card-text">' + obj.recipe[i + 2].description  + '</p></div><div class="card-footer text-center"><a href="#" class="btn btn-success">View</a></div></div></div></div><br>';
+    var cards = []
+
+    for(var i = 0; i < obj.length; i++) {
+        if(!cards.includes(obj[i].name)) {
+            if(obj[i].tag.some(r=> ing_list.indexOf(r) >= 0)) {
+                cards.push(obj[i].name);
+                document.getElementById('list').innerHTML += '<div class="card" style="width: 18rem;"><img src="' + obj[i].image + '" class="card-img-top" alt="..."><div class="card-body">' +
+                  '<h5 class="card-title"><a href="#" class="stretched-link link-dark" style="text-decoration: none;">' + obj[i].name + '</a></h5><p class="card-text">' + obj[i].description + '</p></div></div>';
+            }
+        }
     }
 }
 
