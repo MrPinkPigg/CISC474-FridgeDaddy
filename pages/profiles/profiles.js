e3dfa9330e6490ce973207da328c5f0be9d54047
mrpinkpigg <aaronknestaut@gmail.com>
2021-12-07 09:44:07 -0500

    temp fix for the infinite reloading

--- pages/profiles/profiles.js
+++ pages/profiles/profiles.js
@@ -6,11 +6,11 @@ firebase.auth().onAuthStateChanged((user) => {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
     var uid = user.uid;
-    window.location.href = "signout.html";
+    //window.location.href = "signout.html";
     // ...
   } else {
     // User is signed out
-    window.location.href = "signIn.html";
+    //window.location.href = "signIn.html";
   }
 });
 
@@ -39,7 +39,6 @@ function signUp() {
     });
 
 }
-
 
 /*
 TEST LOGIN:
