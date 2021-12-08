fbe002654fe62a7feaa9e941a0ad0134a74eea5e
Aidan Chao <achao@udel.edu>
2021-12-07 22:16:19 -0500

    Converting cookbooks to server instead of client use

--- app.js
+++ app.js
@@ -4,54 +4,84 @@ const path = require('path');
 const router = express.Router();
 var admin = require("firebase-admin");
 //update to local path
-var serviceAccount = require("C:/Users/Matthew/Desktop/UDine/CISC474-UDine/fridgedaddy-ud21-firebase-adminsdk-46k31-eeba761a04.json");
+var serviceAccount = require("/Users/aidanchao/fridgedaddy-ud21-firebase-adminsdk-46k31-8ea7205be6.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fridgedaddy-ud21-default-rtdb.firebaseio.com"
 });
 
-router.get('/',function(req, res){
-  res.sendFile(path.join(__dirname+'/public/pages/index.html'));
+app.use(express.json());
+app.use(express.urlencoded());
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/index.html'));
 });
 
-router.get('/search',function(req, res){
-  res.sendFile(path.join(__dirname+'/public/pages/recipe-results.html'));
+router.get('/search', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/recipe-results.html'));
 });
 
-router.get('/recipe',function(req, res){
-  res.sendFile(path.join(__dirname+'/public/pages/recipe.html'));
+router.get('/recipe', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/recipe.html'));
+});
+
+router.get('/cookbook', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/cookbooks.html'));
+});
+
+router.get('/addcookbook', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/add-cookbooks.html'));
 });
 
 //testing
-router.get('/test',function(req, res){
-  res.sendFile(path.join(__dirname+'/public/pages/RandomChina.html'));
+router.get('/test', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/RandomChina.html'));
 });
 
-app.get('/recipes', function(req, res) {
+app.get('/recipes', function (req, res) {
   var ref = admin.database().ref('recipes/recipe/');
-  ref.on("value", function(snapshot) {
+  ref.on("value", function (snapshot) {
     res.send(snapshot.val());
-}, function (error) {
-  console.log("Error: " + error.code);
+  }, function (error) {
+    console.log("Error: " + error.code);
   });
 });
 
-app.get('/tags', function(req, res) {
+app.get('/tags', function (req, res) {
   var dropdown = []
   var ref = admin.database().ref('recipes/recipe/');
-  ref.on("value", function(snapshot) {
-  for(var i = 0; i < snapshot.val().length; i++) {
-    for(var j = 0; j < snapshot.val()[i].tag.length; j++) {
-      if(!dropdown.includes(snapshot.val()[i].tag[j])) {
-        dropdown.push(snapshot.val()[i].tag[j]);
+  ref.on("value", function (snapshot) {
+    for (var i = 0; i < snapshot.val().length; i++) {
+      for (var j = 0; j < snapshot.val()[i].tag.length; j++) {
+        if (!dropdown.includes(snapshot.val()[i].tag[j])) {
+          dropdown.push(snapshot.val()[i].tag[j]);
+        }
       }
-    }
-  } res.send(dropdown);
-}, function (error) {
-  console.log("Error: " + error.code);
+    } res.send(dropdown);
+  }, function (error) {
+    console.log("Error: " + error.code);
   });
 });
+
+app.post('/cookbooks', function (req, res) {
+  var ref = admin.database().ref('mycookbooks/')
+  console.log(req.body);
+  var cookbookName = req.body[0];
+  var cookbookDesc = req.body[1];
+
+  console.log(cookbookName);
+
+  var recipes = req.body.slice(2);
+
+  ref.set ({
+    [cookbookName]: {
+      CookbookDesc: cookbookDesc,
+      Recipes: recipes
+    }
+  });
+  res.send('POST request to the homepage')
+})
 
 app.use('/public', express.static(path.join(__dirname, "public")));
 app.use('/', router);
