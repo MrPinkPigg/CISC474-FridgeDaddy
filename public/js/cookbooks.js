ff1263292ed7366a8d9d3cca96fff53f4aca830e
Aidan Chao <achao@udel.edu>
2021-12-12 15:09:43 -0500

    updating some CSS

--- public/js/cookbooks.js
+++ public/js/cookbooks.js
@@ -35,8 +35,9 @@ function populateCookbooks(obj) {
             div.classList.add("col-md-4");
             var cookbookName = obj[key].Cookbook.CookbookName;
             var cookbookDesc = obj[key].Cookbook.CookbookDesc;
+            var numRecipes = obj[key].Cookbook.Recipes.length - 1;
 
-            var cookbookDiv = '<div class="card mb-4 box-shadow">' + '<img class="card-img-top" src="/public/images/cookbook.jpeg" alt="Image of a cookbook">' + '<div class="card-body">' + '<h5 class="card-title">' + cookbookName + '</h5>' + '<p class="card-text">' + cookbookDesc + '<div class="d-flex justify-content-between align-items-center">' + '<div class="btn-group">' + '<button onclick="myFun(this.id)" id="' + cookbookName + '" type="button" class="btn btn-sm btn-outline-secondary view-cookbook-button">View' + '</button>' + '<button onclick="deleteCookbook(this.id)" id="' + cookbookName + '" type="button" class="btn btn-sm btn-outline-secondary">Delete' + '</button>' + '</div>' + '<small class="text-muted">0 recipes</small>' + '</div>' + '</div>' + '</div>' + '</div>'
+            var cookbookDiv = '<div class="card mb-4 box-shadow">' + '<img class="card-img-top" src="/public/images/cookbook.jpeg" alt="Image of a cookbook">' + '<div class="card-body">' + '<h5 class="card-title">' + cookbookName + '</h5>' + '<p class="card-text">' + cookbookDesc + '<div class="d-flex justify-content-between align-items-center">' + '<div class="btn-group">' + '<button onclick="myFun(this.id)" id="' + cookbookName + '" type="button" class="btn btn-sm btn-outline-secondary view-cookbook-button">View' + '</button>' + '<button onclick="deleteCookbook(this.id)" id="' + cookbookName + '" type="button" class="btn btn-sm btn-outline-secondary">Delete' + '</button>' + '</div>' + '<small class="text-muted">' + numRecipes +  ' recipes</small>' + '</div>' + '</div>' + '</div>' + '</div>'
 
             div.innerHTML = cookbookDiv;
             parent.appendChild(div);
