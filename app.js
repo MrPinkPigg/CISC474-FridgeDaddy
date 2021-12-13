683a2ffcbed8527b280c0399eaa138b3c3918f8e
sunxinyi0501 <sunxinyi@udel.edu>
2021-12-13 18:52:07 -0500

    link update and some css change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = express.Router();
 var admin = require("firebase-admin");
 //update to local path
-var serviceAccount = require("C:/Users/Aaron Knestaut/Documents/College/cisc474/CISC474-FridgeDaddy/fridgedaddy-ud21-firebase-adminsdk-46k31-bffc819e83.json");
+var serviceAccount = require("D:/cisc474/group/fridgedaddy-ud21-firebase-adminsdk-46k31-d8e3282848.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
